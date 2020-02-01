b02b02f938560e303e3b5d12bb46a3ea49bee4a7
Joshua Morgan <jpmorgan.digital@hgmail.com>
2020-01-31 20:38:55 -0800

    Setting up initial Unity Project

---
+++ EcoLand_client/Assets/WorldGen.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class WorldGen : MonoBehaviour
+{
+    public GameObject CellPrefab;
+
+    public int worldSize;
+    
+    public void Awake()
+    {
+        for(int x = 0; x < worldSize; x++)
+        {
+            for (int y = 0; y < worldSize; y++)
+            {
+                GameObject.Instantiate(CellPrefab);
+            }
+        }
+    }
+}
+
+
+public class Cell
+{
+    private Dictionary<Direction, Cell> neighbors = new Dictionary<Direction, Cell>();
+    
+    public void SetNeighbor(Direction dir, Cell neighbor)
+    {
+        
+    }
+}
+
+public enum Direction
+{
+    North,
+    West,
+    South,
+    East,
+    NorthWest,
+    NorthEast,
+    SouthWest,
+    SouthEast
+}
+
+public class WorldInstance
+{
+
+}
