c5f87b8b5e0c5e69ebd5cab638054ae7b4f5fec8
Joshua Morgan <jpmorgan.digital@hgmail.com>
2020-02-01 01:49:29 -0800

    Bad cam

---
+++ EcoLand_client/Assets/Controls.cs
@@ -0,0 +1,191 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Controls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Controls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controls"",
+    ""maps"": [
+        {
+            ""name"": ""GamePlay"",
+            ""id"": ""d5deec6d-8fc2-4dda-b876-e4b0fb6b58f9"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""0d64ecf8-41ae-4538-87cf-9ca8a88cddcd"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""a5d25adc-60b3-47e5-9b72-8316e2ff0361"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": ""Hold(duration=1000)"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""39ac0ca3-7873-41d3-9932-3c8159c31c77"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""98a01776-a31c-4a57-8e59-5bcdf98abb5c"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""335a9e35-f75f-49f5-8e08-70e77f925ae3"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""d3971a8f-28ce-452b-9203-a566a96cd4f3"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Default"",
+            ""bindingGroup"": ""Default"",
+            ""devices"": []
+        }
+    ]
+}");
+        // GamePlay
+        m_GamePlay = asset.FindActionMap("GamePlay", throwIfNotFound: true);
+        m_GamePlay_Move = m_GamePlay.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // GamePlay
+    private readonly InputActionMap m_GamePlay;
+    private IGamePlayActions m_GamePlayActionsCallbackInterface;
+    private readonly InputAction m_GamePlay_Move;
+    public struct GamePlayActions
+    {
+        private @Controls m_Wrapper;
+        public GamePlayActions(@Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_GamePlay_Move;
+        public InputActionMap Get() { return m_Wrapper.m_GamePlay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GamePlayActions set) { return set.Get(); }
+        public void SetCallbacks(IGamePlayActions instance)
+        {
+            if (m_Wrapper.m_GamePlayActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_GamePlayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public GamePlayActions @GamePlay => new GamePlayActions(this);
+    private int m_DefaultSchemeIndex = -1;
+    public InputControlScheme DefaultScheme
+    {
+        get
+        {
+            if (m_DefaultSchemeIndex == -1) m_DefaultSchemeIndex = asset.FindControlSchemeIndex("Default");
+            return asset.controlSchemes[m_DefaultSchemeIndex];
+        }
+    }
+    public interface IGamePlayActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
---
+++ EcoLand_client/Assets/PlayerCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private PlayerInput Input;
+
+    public Controls input;
+
+    public Vector3 Velocity;
+    public float acceleration = 5.0f;
+    private Camera _camera;
+
+    public void Awake()
+    {
+        Input = GetComponent<PlayerInput>();
+        
+        _camera = GetComponent<Camera>();
+
+        input = new Controls();;
+        input.Enable();
+        input.GamePlay.Move.Enable();
+    }
+
+    public void Update()
+    {
+        var move = -input.GamePlay.Move.ReadValue<Vector2>() * acceleration * Time.deltaTime;
+
+        var cameraRight = Vector3.Cross(_camera.transform.forward, Vector3.up);
+        var cameraForward = Vector3.Cross(cameraRight, Vector3.up);
+
+        var right = move.x * cameraRight;
+        var forward = move.y * cameraForward;
+        
+        Velocity += right + forward ;
+
+        Velocity -= Velocity * 0.95f * Time.deltaTime;
+        
+        transform.position = transform.position + Velocity * Time.deltaTime;
+    }
+}
