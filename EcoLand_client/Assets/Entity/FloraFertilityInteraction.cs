10ad3a430ca836e51af45e5ea00a23181bc2cdfd
TheUltimateMuffin <ryan.theultimatemuffin@gmail.com>
2020-02-02 12:46:34 -0800

    added starvationRate to floraFertilityInteraction

--- EcoLand_client/Assets/Entity/FloraFertilityInteraction.cs
+++ EcoLand_client/Assets/Entity/FloraFertilityInteraction.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 namespace EntitySystem
 {
+    
     public class FloraFertilityInteraction : BaseComponent, IStatusStep
     {
         public float fertilityConsumptionRate = 0.001f;
         public float minFertility = 0f; // Minimum fertility needed for plant to grow and survive
+
+        public int starvationRate = 5;
 
         public void StatusStep() {
             if (entity.currentTile.fertility > minFertility) {
                 entity.fertilityReservoir += fertilityConsumptionRate;
                 entity.currentTile.fertility -= fertilityConsumptionRate;
             } else {
-                entity.deathAge -= 10;
+                entity.deathAge -= starvationRate;
             }
         }
     }
