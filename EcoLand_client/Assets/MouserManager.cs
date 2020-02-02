731d753d47760cf0e3012870540970e99142ac2f
Joshua Morgan <jpmorgan.digital@hgmail.com>
2020-02-02 09:32:30 -0800

    gamey gamey game game

---
+++ EcoLand_client/Assets/MouserManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouserManager : MonoBehaviour
+{
+    public Mouser mouser;
+
+    public void SetPlayerTool(Tool tool)
+    {
+        if (tool == null)
+        {
+            mouser.gameObject.SetActive(false);
+        }
+        else
+        {
+            mouser.gameObject.SetActive(true);
+
+            mouser.img = tool.mouseIcon;
+        }
+    }
+}
