9e99e52ad0aae5547dce87181cdcd89f56c33227
TheUltimateMuffin <ryan.theultimatemuffin@gmail.com>
2020-02-01 17:18:27 -0800

    Dryness/wetness/fertility terrain system first pass.

--- EcoLand_client/Assets/TileMechanics/WorldTile.cs
+++ EcoLand_client/Assets/TileMechanics/WorldTile.cs
@@ -10,12 +10,12 @@ namespace Josh
         private const float temperatureSpaceRadiationSpeed =    0.001f;
         private const float temperatureSunWarmingSpeed =        0.001f;
 
-        private const float waterTileFlowSpeed =                0.02f;
+        private const float waterTileFlowSpeed =                0.01f;
         private const float waterFreezingTemperature =          0.32f;
         private const float waterEvaporationSpeed =             0.001f;
 
         private const float rainfallSpeed =                     0.001f;
-        private const float humidityTransferSpeed =             0.001f;
+        private const float humidityTransferSpeed =             0.02f;
 
         private Material cellMaterial;
 
@@ -71,19 +71,20 @@ namespace Josh
         private void RunHydrationSimulation() {
             // Water bleeds out, but only downhill, and only if it's warm enough
             float waterFlowLoss = 0f;
-            if (temperature > waterFreezingTemperature) {
-                float waterLossToAdjacentTile = hydration * waterTileFlowSpeed;
-                foreach (Cell neighbor in myCell.GetNeighbors().Values) {
-                    if (neighbor.GetWorldTile().elevation - elevation > 0) {
-                        float tileFlowLoss = waterLossToAdjacentTile * (neighbor.GetWorldTile().elevation - elevation);
-                        neighbor.GetWorldTile().hydration += tileFlowLoss;
-                        waterFlowLoss += tileFlowLoss;
-                    } else if(hydration > 1f && neighbor.GetWorldTile().elevation - (elevation + (1-hydration)) > 0) {
-                        // hydration > 1 means underwater tile so add additional hydration to elevation for flow
-                        float tileFlowLoss = waterLossToAdjacentTile * (neighbor.GetWorldTile().elevation - (elevation + (1 - hydration)));
-                        neighbor.GetWorldTile().hydration += tileFlowLoss;
-                        waterFlowLoss += tileFlowLoss;
-                    }
+            float waterLossToAdjacentTile = hydration * waterTileFlowSpeed;
+            if (temperature < waterFreezingTemperature)
+                waterLossToAdjacentTile *= 0.1f; // if the water is frozen less will flow
+
+            foreach (Cell neighbor in myCell.GetNeighbors().Values) {
+                if (elevation - neighbor.GetWorldTile().elevation > 0) {
+                    float tileFlowLoss = waterLossToAdjacentTile * (elevation - neighbor.GetWorldTile().elevation);
+                    neighbor.GetWorldTile().hydration += tileFlowLoss;
+                    waterFlowLoss += tileFlowLoss;
+                } else if(hydration > 1f && (elevation + (1-hydration)) - neighbor.GetWorldTile().elevation > 0) {
+                    // hydration > 1 means underwater tile so add additional hydration to elevation for flow
+                    float tileFlowLoss = waterLossToAdjacentTile * ((elevation + (1 - hydration) - neighbor.GetWorldTile().elevation));
+                    neighbor.GetWorldTile().hydration += tileFlowLoss;
+                    waterFlowLoss += tileFlowLoss;
                 }
             }
 
@@ -117,6 +118,7 @@ namespace Josh
 
             cellMaterial.SetFloat("Vector1_hydration", Mathf.Clamp(hydration - 1, 0f, 1f));
             cellMaterial.SetFloat("Vector1_fertility", fertility);
+            cellMaterial.SetFloat("Vector1_sand", Mathf.Clamp(0.1f - hydration, 0f, 1f));
 
 
             return;//TODO
